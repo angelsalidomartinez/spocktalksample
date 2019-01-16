105858feb49e641df3a0a9671cfdb11b31f5b82e
angelsalidomartinez <angel.salido.martinez@gmail.com>
2019-01-16 08:22:42 +0100

    [SpockTalkSample]first commit and push

---
+++ src/main/java/com/angel/spock/spocktalksample/SpocktalksampleApplication.java
@@ -0,0 +1,14 @@
+package com.angel.spock.spocktalksample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpocktalksampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpocktalksampleApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/angel/spock/spocktalksample/SpocktalksampleApplicationTests.java
@@ -0,0 +1,17 @@
+package com.angel.spock.spocktalksample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpocktalksampleApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
